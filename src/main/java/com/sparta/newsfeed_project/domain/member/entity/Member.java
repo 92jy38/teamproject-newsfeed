4cef5c273d97484f971e30808ef527c7dcd8de8e
jubseok <junseok708@gmail.com>
2024-10-21 12:10:36 +0900

    Feat : entity 생성 , buddy와 join하기  위해 생성 필요 컬럼 생성

--- src/main/java/com/sparta/newsfeed_project/domain/member/entity/Member.java
+++ src/main/java/com/sparta/newsfeed_project/domain/member/entity/Member.java
@@ -1,4 +1,13 @@
 package com.sparta.newsfeed_project.domain.member.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
 }
