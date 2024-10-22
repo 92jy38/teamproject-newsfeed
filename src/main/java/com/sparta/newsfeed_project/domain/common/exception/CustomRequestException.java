d9b7edc661c9e7a3d07f211087aa374a046f9e5a
34-43 <jehyuck3443@gmail.com>
2024-10-22 17:36:32 +0900

    fix: 전역 예외 처리기 중복 삭제 (member 브랜치에서 예약)

--- src/main/java/com/sparta/newsfeed_project/domain/common/exception/CustomRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.newsfeed_project.domain.common.exception;
-
-public class CustomRequestException extends RuntimeException {
-    public CustomRequestException(String message) {
-        super(message);
-    }
-}
