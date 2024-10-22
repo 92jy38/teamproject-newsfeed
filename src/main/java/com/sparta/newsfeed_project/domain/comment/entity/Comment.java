3700884fb31510bed1a93e087fbbf4376e9cf18a
92jy38 <92jy38@gmail.com>
2024-10-22 22:47:44 +0900

    refactor: Timestamped 사용으로 인한 수정

--- src/main/java/com/sparta/newsfeed_project/domain/comment/entity/Comment.java
+++ src/main/java/com/sparta/newsfeed_project/domain/comment/entity/Comment.java
@@ -1,20 +1,19 @@
 package com.sparta.newsfeed_project.domain.comment.entity;
 
+import com.sparta.newsfeed_project.domain.common.entity.Timestamped;
 import com.sparta.newsfeed_project.domain.member.entity.Member;
 import com.sparta.newsfeed_project.domain.post.entity.Post;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-import java.time.LocalDateTime;
+
 
 @Entity
 @Getter
 @NoArgsConstructor
 @Table(name = "comments")
-public class Comment {
+public class Comment extends Timestamped {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,14 +32,6 @@ public class Comment {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id", nullable = false)
     private Post post;
-
-    @CreationTimestamp
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
-
-    @UpdateTimestamp
-    @Column(nullable = false)
-    private LocalDateTime updatedAt;
 
     // setter 대신 생성자 사용
     public Comment(String content, Member member, Post post) {
