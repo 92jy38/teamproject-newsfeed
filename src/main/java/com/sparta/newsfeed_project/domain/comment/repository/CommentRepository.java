0db3bf85c85aa85cd4944ae2856306eddcea8741
92jy38 <92jy38@gmail.com>
2024-10-23 01:28:00 +0900

    feat: @EntityGraph 사용으로 N+1 문제 해결

--- src/main/java/com/sparta/newsfeed_project/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/sparta/newsfeed_project/domain/comment/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package com.sparta.newsfeed_project.domain.comment.repository;
 
 import com.sparta.newsfeed_project.domain.comment.entity.Comment;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,7 @@ import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    // EntityGraph를 사용하여 Member 엔티티를 함께 조회
+    @EntityGraph(attributePaths = {"member"})
     List<Comment> findByPostId(Long postId);
 }
