a1b8dc8402652a168004a23b1372e93dd8207f06
jubseok <junseok708@gmail.com>
2024-10-22 15:12:10 +0900

    fet : post 요구 사항 , memberId으로 맞팔인 친구 리스트 찾기

--- src/main/java/com/sparta/newsfeed_project/domain/buddies/repository/BuddiesRepository.java
+++ src/main/java/com/sparta/newsfeed_project/domain/buddies/repository/BuddiesRepository.java
@@ -2,11 +2,13 @@ package com.sparta.newsfeed_project.domain.buddies.repository;
 
 import com.sparta.newsfeed_project.domain.buddies.entity.Buddies;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface BuddiesRepository extends JpaRepository <Buddies,Long> {
+public interface BuddiesRepository extends JpaRepository<Buddies, Long> {
 
     List<Buddies> findAllByFromUserIdOrToUserId(Long memberId, Long buddyId);
 
@@ -15,4 +17,9 @@ public interface BuddiesRepository extends JpaRepository <Buddies,Long> {
     List<Buddies> findAllByToUserId(Long memberId);
 
     boolean existsByFromUserIdAndToUserId(Long fromUserId, Long toUserId);
+
+    @Query("select b.fromUserId from Buddies b where b.toUserId = :memberId and b.approved = true ")
+    List<Long> findAllIdByFromUserId(@Param("memberId") Long memberId);
+
+
 }
