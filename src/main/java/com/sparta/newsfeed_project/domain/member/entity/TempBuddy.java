01198a8feabdffa026606b0f29901f7467cab8bb
guguggagga <gaeul79_@naver.com>
2024-10-22 19:02:22 +0900

    fix: 합치기 앞서 문제될만한 코드는 일단 주석

--- src/main/java/com/sparta/newsfeed_project/domain/member/entity/TempBuddy.java
+++ src/main/java/com/sparta/newsfeed_project/domain/member/entity/TempBuddy.java
@@ -1,28 +1,28 @@
-package com.sparta.newsfeed_project.domain.member.entity;
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "buddy")
-public class TempBuddy {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column()
-    private Long fromId;
-
-    @Column()
-    private Long toId;
-
-    @Column()
-    private boolean state;
-}
+//package com.sparta.newsfeed_project.domain.member.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Getter
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Entity
+//@Table(name = "buddy")
+//public class TempBuddy {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column()
+//    private Long fromId;
+//
+//    @Column()
+//    private Long toId;
+//
+//    @Column()
+//    private boolean state;
+//}
