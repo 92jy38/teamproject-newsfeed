93b5bd398346eb512d843d9a7f2cfac6dcfeb587
jubseok <junseok708@gmail.com>
2024-10-22 11:35:11 +0900

    fix : BuddyDto 추가 , 왜 사용하는지 모르겠다

---
+++ src/main/java/com/sparta/newsfeed_project/domain/buddies/dto/BuddyDto.java
@@ -0,0 +1,25 @@
+package com.sparta.newsfeed_project.domain.buddies.dto;
+
+import com.sparta.newsfeed_project.domain.buddies.entity.Buddies;
+import jdk.jshell.Snippet;
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BuddyDto {
+    private Long id;
+    private Long fromUesrId;
+    private Long toUserId;
+    private boolean approved;
+
+    public static BuddyDto from(Buddies buddies) {
+        return BuddyDto.builder()
+                .id(buddies.getId())
+                .fromUesrId(buddies.getFromUesrId())
+                .toUserId(buddies.getToUserId())
+                .approved(buddies.isAppoved()).build();
+    }
+
+}
