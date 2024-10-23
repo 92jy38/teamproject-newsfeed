d862768f6b8614c2c5b538feba9bca2f1ed70adf
guguggagga <gaeul79_@naver.com>
2024-10-23 19:46:50 +0900

    fix: 프로필 조회 버그 수정

--- src/main/java/com/sparta/newsfeed_project/domain/common/exception/ResponseCode.java
+++ src/main/java/com/sparta/newsfeed_project/domain/common/exception/ResponseCode.java
@@ -16,7 +16,6 @@ public enum ResponseCode {
     // 로그인 관련 코드
     SUCCESS_LOGIN(HttpStatus.OK, "로그인 성공"),
     SUCCESS_LOGOUT(HttpStatus.OK, "로그 아웃 성공"),
-    INVALID_PERMISSION(HttpStatus.FORBIDDEN, "권한이 없습니다"),
 
     // 토큰 관련 에러 코드
     TOKEN_UNSIGNED(HttpStatus.BAD_REQUEST, "유효하지 않는 JWT 서명 입니다."),
@@ -45,11 +44,6 @@ public enum ResponseCode {
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다."),
     COMMENT_INVALID_PERMISSION(HttpStatus.FORBIDDEN, "댓글에 대한 권한이 없습니다"),
     UNAUTHORIZED(HttpStatus.FORBIDDEN, "로그인 상태를 확인 바랍니다"),
-
-    // TODO 보완 필요 DB & 서버 관련 에러 코드
-    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB 에러가 발생 하였습니다."),
-    UNKNOWN_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 오류"),
-
 
     // 기타 에러 코드
     BAD_INPUT(HttpStatus.BAD_REQUEST, "잘못된 값 입력"),
