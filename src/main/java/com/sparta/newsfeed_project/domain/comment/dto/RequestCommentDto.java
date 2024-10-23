e169ba405d530b37c2201af769c73a4ed758f89e
92jy38 <92jy38@gmail.com>
2024-10-23 15:36:39 +0900

    refactor: 토큰 사용으로 인한 객체 사용으로 필드 삭제

--- src/main/java/com/sparta/newsfeed_project/domain/comment/dto/RequestCommentDto.java
+++ src/main/java/com/sparta/newsfeed_project/domain/comment/dto/RequestCommentDto.java
@@ -1,7 +1,6 @@
 package com.sparta.newsfeed_project.domain.comment.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
@@ -11,9 +10,4 @@ public class RequestCommentDto {
     @NotBlank(message = "댓글 내용은 필수입니다.")
     @Size(min = 1, max = 255, message = "댓글은 1자 이상 255자 이하만 가능합니다")
     private String content;
-
-    // CommentService 에서 memberId를 호출하기 때문에 다시 주석처리 해제
-    @NotNull(message = "작성자 ID는 필수입니다.")
-    private Long memberId;
-
 }
