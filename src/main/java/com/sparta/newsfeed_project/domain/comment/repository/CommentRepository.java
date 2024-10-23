0013fd708b8c83c52ed3ecfa1e59c86753348a10
92jy38 <92jy38@gmail.com>
2024-10-23 10:05:32 +0900

    feat: 댓글 조회 페이징 처리 추가로 인한 수정

--- src/main/java/com/sparta/newsfeed_project/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/sparta/newsfeed_project/domain/comment/repository/CommentRepository.java
@@ -3,13 +3,12 @@ package com.sparta.newsfeed_project.domain.comment.repository;
 import com.sparta.newsfeed_project.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    // EntityGraph를 사용하여 Member 엔티티를 함께 조회
-    @EntityGraph(attributePaths = {"member"})
-    List<Comment> findByPostId(Long postId);
+    @EntityGraph(attributePaths = {"member"}) // EntityGraph를 사용하여 Member 엔티티를 함께 조회
+    Page<Comment> findByPostId(Long postId, Pageable pageable); // 페이지 사용
 }
