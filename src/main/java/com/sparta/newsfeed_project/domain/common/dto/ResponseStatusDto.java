e9cfc0e6d6d530685f2fd24b31ee6d6d8b35ed0d
guguggagga <gaeul79_@naver.com>
2024-10-24 10:22:02 +0900

    fix: 성공 응답 코드 제거 - 성공 코드 모두 제거 - 이미지 관련 에러코드 하나 추가

--- src/main/java/com/sparta/newsfeed_project/domain/common/dto/ResponseStatusDto.java
+++ src/main/java/com/sparta/newsfeed_project/domain/common/dto/ResponseStatusDto.java
@@ -4,6 +4,7 @@ import com.sparta.newsfeed_project.domain.common.exception.ResponseCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
 
 /**
  * API응답 상태에 대한 정보를 제공하는 DTO 클래스
@@ -25,5 +26,10 @@ public class ResponseStatusDto {
     public ResponseStatusDto(ResponseCode responseCode, String message) {
         state = responseCode.getHttpStatus().value();
         this.message = responseCode.getMessage() + ": " + message;
+    }
+
+    public ResponseStatusDto(HttpStatus httpStatus, String message) {
+        state = httpStatus.value();
+        this.message = message;
     }
 }
